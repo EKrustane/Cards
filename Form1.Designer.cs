fa49922930e8e26e71f823529dc576382dd5539c
Ermine <ermux12@gmail.com>
2020-08-05 16:58:40 +0300

    version 16:58

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Cards
 {
-    partial class Form1
+    partial class Desk
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Desk));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.LoadCards = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LoadCards});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 38);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // LoadCards
+            // 
+            this.LoadCards.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.LoadCards.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadCards.Image = ((System.Drawing.Image)(resources.GetObject("LoadCards.Image")));
+            this.LoadCards.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.LoadCards.Name = "LoadCards";
+            this.LoadCards.Size = new System.Drawing.Size(105, 33);
+            this.LoadCards.Text = "Load Cards";
+            this.LoadCards.Click += new System.EventHandler(this.LoadCards_Click);
+            // 
+            // Desk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Desk";
             this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton LoadCards;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Cards
 {
-    public partial class Form1 : Form
+    public partial class Desk : Form
     {
-        public Form1()
+        private string folderPath = null;
+        private string[] fileNames = null; 
+
+        public Desk()
         {
             InitializeComponent();
+            InitializeDesk();
+        }
+
+        private void InitializeDesk()
+        {
+            this.BackColor = Color.Black;
+        }
+
+        private void LoadCards_Click(object sender, EventArgs e)
+        {
+            PictureBox filePictureBox = null;
+            folderPath = @"C:\Users\asus\Desktop\Playing Cards\Playing Cards\playing_card_images\face";
+            fileNames = Directory.GetFiles(folderPath);
+
+            foreach(var fileName in fileNames)
+            {
+                filePictureBox = new PictureBox()
+                {
+                    Height = 100,
+                    Width = 70,
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    Left = 100,
+                    Top = 100,
+                    Image = Image.FromFile(fileName)
+                };
+                this.Controls.Add(filePictureBox);
+            }
         }
     }
 }
